ca91cff8fb801b428e0b3bda96e46dfe85d5097e
mohamed mohsen <mmohsen547@gmail.com>
2021-09-10 15:59:15 +0200

    First commit

---
+++ DoctorAppointmentSystem/Controllers/DoctorRegisterationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoctorAppointmentSystem.Controllers
+{
+    public class DoctorRegisterationController : Controller
+    {
+        // GET: DoctorRegisteration
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
